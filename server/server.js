49be8694dbe451690f842dc8e0e06f0197e85e27
Anirudh Umarji <anirudh.umarji@gmail.com>
2021-07-15 11:45:43 +0530

    Heroku-build

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require( 'path' );
 const app = express();
 
 const publicPath = path.join( __dirname, '..', 'dist' );
+const port = process.env.PORT || 3000;
 
 app.use( express.static( publicPath ) );
 app.get( '*', ( req, res ) =>
@@ -12,7 +13,7 @@ app.get( '*', ( req, res ) =>
     res.sendFile( path.join( publicPath, 'index.html' ) );
 } );
 
-app.listen( 3000, () =>
+app.listen( port, () =>
 {
     console.log( 'Server Running' );
 } );
